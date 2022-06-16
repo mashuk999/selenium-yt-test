6ac607250cf16de2edc11903f48b87bced2cdccd
mashuk999 <mashuk1998@gmail.com>
2022-06-17 00:42:10 +0530

    added video

--- yttest.py
+++ yttest.py
@@ -10,13 +10,13 @@ from multiprocessing import Process, freeze_support
 if __name__ == "__main__":
     freeze_support()  # needed for Windows
 
-    chrome = Chrome(headless=True)
+    chrome = Chrome(headless=True,profile_path=r"/home/runner/work/selenium-yt-test/selenium-yt-test/selenium-python-profile/")
     #chrome.add_cookie({"name": "key", "value": "value"})
 
     youtube = Youtube(
         browser=chrome # or firefox
     )
     print('1')
-    upload_result = youtube.upload('path_to_video', 'title', 'description', ['tag1', 'tag2'])
+    upload_result = youtube.upload(r"/home/runner/work/selenium-yt-test/selenium-yt-test/video.mp4", 'title', 'description', ['tag1', 'tag2'])
     print('2')
     input("Press Enter to continue...")
