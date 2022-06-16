9b02e14575253a2311eb94ba36854714a6fb3e85
Abhay Nigam <36666689+mashuk999@users.noreply.github.com>
2022-06-17 01:35:47 +0530

    Update yttest.py

--- yttest.py
+++ yttest.py
@@ -10,13 +10,13 @@ from multiprocessing import Process, freeze_support
 if __name__ == "__main__":
     freeze_support()  # needed for Windows
 
-    chrome = Chrome(headless=True,profile_path=r"/home/runner/work/selenium-yt-test/selenium-yt-test/selenium-python-profile/")
+    chrome = Chrome(headless=True,profile_path=r"D:/a/selenium-yt-test/selenium-yt-test/selenium-python-profile/")
     #chrome.add_cookie({"name": "key", "value": "value"})
 
     youtube = Youtube(
         browser=chrome # or firefox
     )
     print('1')
-    upload_result = youtube.upload(r"/home/runner/work/selenium-yt-test/selenium-yt-test/video.mp4", 'title1', 'description', ['tag', 'tag2'])
+    upload_result = youtube.upload(r"D:/a/selenium-yt-test/selenium-yt-test/video.mp4", 'title1', 'description', ['tag', 'tag2'])
     print('2')
     input("Press Enter to continue...")
